ad5118132d4c462f47ff700d8d1bb8ec91a49f75
PizzaDimas <148796907+PizzaDimas@users.noreply.github.com>
2023-11-05 00:45:19 +0300

    Add files via upload

--- Capital.java
+++ Capital.java
@@ -19,15 +19,8 @@ public class Capital {
      * максимальную процентную ставку изъятия капитала, при которой владелец капитала гарантированно
      * сможет покрывать свои базовые потребности до 2022 года.
      */
-    private double amountOfCapital = 100;                          // размер капитала
+
     private int startYear;                                         // первый год жизни с капиталом
-
-    public Capital(int startYear) {
-        this.startYear = startYear;
-    }
-
-    public Capital() {
-    }
 
     public int getStartYear() throws Exception {
         // здесь идет реализация ввода года начала жизни на проценты с консоли
@@ -38,45 +31,54 @@ public class Capital {
         } catch (Exception e) {
             throw new Exception("acceptable interval: [2002-2021]");
         }
-
     }
 
-    public boolean checkYear(int startYear) {
+    private boolean checkYear(int startYear) {
         // здесь идет реализация статуса соотвествия введенного года начала на соответсвие допустимому интервалу.
-        return (((startYear < 2022) && (startYear > 2001)) ? true : false);
+        return ((startYear < 2022) && (startYear > 2001));
     }
 
     public void enterYearAndCalculate() throws Exception {
         // здесь идет проверка статуса соответсвия введенного года, а также печать результатов главного метода
         try {
-            if (!checkYear(getStartYear())) throw new Exception("acceptable interval: [2002-2021]");
+            if (!checkYear(getStartYear())) {
+                throw new Exception("acceptable interval: [2002-2021]");
+            }
             System.out.println(findMaxPercent());
         } catch (Exception e) {
             System.out.println("Error! " + e.getMessage());
         }
     }
 
-    public double findMaxPercent() {
-        // здесь реализован главный метод класса, поиск максимальной процентной ставки для соответсвия условиям задачи
-        double percent = 0.5;                                                                                  // Процентная ставка
-        double baseExpenses = percent / 100 * amountOfCapital;                                                 // Базовые расходы, которые составляют изначально 4% от капитала
-        // цикл увеличивает процентную ставку на 0.5 в случае успешной итерации
-        for (int i = 0; (amountOfCapital > 0); i++) {
+    private double passCapitalThroughYear(double amountOfCapital, double baseExpenses, int j){
+        amountOfCapital = amountOfCapital - baseExpenses;                                              // В начале года происходит списание расходов на год
+        amountOfCapital = amountOfCapital                                                              // Индексация капитала на индекс биржи
+                * (Constants.MOEX_RATE[startYear - 2002 + 1 + j]
+                / Constants.MOEX_RATE[startYear - 2002 + j]);
+        return amountOfCapital;
+    }
 
-            // цикл уменьшает каждый год капитал на размер базовых расходов, проводит индексацию по индексу мос биржи и инфляции
-            for (int j = 0; j < (2022 - startYear); j++) {
-                amountOfCapital = amountOfCapital - baseExpenses;                                              // В начале года происходит списание расходов на год
-                amountOfCapital = amountOfCapital                                                              // Индексация капитала на индекс биржи
-                        * (Constants.MOEX_RATE[startYear - 2002 + 1 + j]
-                        / Constants.MOEX_RATE[startYear - 2002  + j]);
-                baseExpenses = baseExpenses * (1 + (Constants.INFLATION_RATE[startYear - 2002 + j]) / 100);    // Индексация базовых расходов на инфлюцию в конце года
-                if (amountOfCapital < 0) return (percent - 0.5);                                               // При остановке цикла изза истощения капитала возвращается предыдущее значение ставки
-            }
+    private double indexBaseExpenses(double baseExpenses, int j){
+        return (baseExpenses * (1 + (Constants.INFLATION_RATE[startYear - 2002 + j]) / 100));          // Индексация базовых расходов на инфлюцию в конце года
+    }
+    public double findMaxPercent() {
+        // здесь реализован главный метод класса, поиск максимальной процентной ставки
+        double percent;                                                                                // Процентная ставка
+        double amountOfCapital;
+        double baseExpenses;
+        // цикл увеличивает процентную ставку на 0.5 в случае успешной итерации
+        for (percent = 0.5; (true); percent += 0.5) {
             // условия для итерации с новой процентной ставкой
             amountOfCapital = 100;
-            percent += 0.5;
-            baseExpenses = percent / 100 * amountOfCapital;
+            baseExpenses = percent / 100 * amountOfCapital;                                            // Базовые расходы, которые составляют изначально 4% от капитала
+            // цикл уменьшает каждый год капитал на размер базовых расходов, проводит индексацию
+            for (int j = 0; j < (2022 - startYear); j++) {
+                amountOfCapital = passCapitalThroughYear(amountOfCapital, baseExpenses, j);
+                baseExpenses = indexBaseExpenses(baseExpenses, j);
+                if (amountOfCapital < 0) {
+                    return (percent - 0.5);                                                             // При остановке цикла изза истощения капитала возвращается предыдущее значение ставки
+                }
+            }
         }
-        return percent;
     }
 }
